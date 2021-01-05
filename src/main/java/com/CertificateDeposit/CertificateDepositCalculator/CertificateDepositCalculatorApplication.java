c6eb225fb1b6257f7b16e452b796e3340a9daab0
John Frocillo <jcf10123@comcast.net>
2021-01-05 15:00:45 -0500

    Initial Commit and Spring Initializer

---
+++ src/main/java/com/CertificateDeposit/CertificateDepositCalculator/CertificateDepositCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.CertificateDeposit.CertificateDepositCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateDepositCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateDepositCalculatorApplication.class, args);
+	}
+
+}
