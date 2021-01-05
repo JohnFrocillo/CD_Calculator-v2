3728299d4fd24cc7a1299a6cc631921334e082d2
John Frocillo <jcf10123@comcast.net>
2021-01-05 15:44:19 -0500

    Added GET mapping that takes in P,r,n,t and returns A for interest formula

--- src/main/java/com/CertificateDeposit/CertificateDepositCalculator/Controller.java
+++ src/main/java/com/CertificateDeposit/CertificateDepositCalculator/Controller.java
@@ -1,7 +1,10 @@
 package com.CertificateDeposit.CertificateDepositCalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CertificateDeposit.CertificateDepositCalculator.Model.InterestFormula;
 
 @RestController
 public class Controller {
@@ -9,6 +12,11 @@ public class Controller {
     @GetMapping("/test")
 	public String testMethod() {
 		return "Success!";
+    }
+
+    @GetMapping("/Calculation")
+    public String calculateCDValue(@RequestBody InterestFormula values) {
+        return Double.toString(values.getA());
     }
     
 }
---
+++ src/main/java/com/CertificateDeposit/CertificateDepositCalculator/Model/InterestFormula.java
@@ -0,0 +1,68 @@
+package com.CertificateDeposit.CertificateDepositCalculator.Model;
+
+public class InterestFormula {
+    private double A; // Final amount
+    private double P; // Initial principal balance
+    private double r; // Interest rate
+    private double n; // Number of times interest applied per time period
+    private double t; // Number of time periods elapsed
+
+    public InterestFormula(double P, double r, double n, double t) {
+        this.P = P;
+        this.r = r;
+        this.n = n;
+        this.t = t;
+
+        this.A = calculateResult(P,r,n,t);
+    }
+
+    // Use formula A = P(1+r/n)^(nt)
+    private double calculateResult(double P, double r, double n, double t) {
+        double result;
+        result = P * Math.pow(1 + (r/n), n*t);
+
+        return result;
+    }
+
+    public double getA() {
+        return this.A;
+    }
+
+    public void setA(double A) {
+        this.A = A;
+    }
+
+    public double getP() {
+        return this.P;
+    }
+
+    public void setP(double P) {
+        this.P = P;
+    }
+
+    public double getR() {
+        return this.r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getN() {
+        return this.n;
+    }
+
+    public void setN(double n) {
+        this.n = n;
+    }
+
+    public double getT() {
+        return this.t;
+    }
+
+    public void setT(double t) {
+        this.t = t;
+    }
+
+
+}
